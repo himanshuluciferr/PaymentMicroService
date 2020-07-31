e372c35724dcad526be1a694871f6919075fcd35
Groupcreation <groupcreation100@gmail.com>
2020-07-31 14:58:58 +0530

    name change

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Payment.Model;
+
+namespace Payment.Controllers
+
+{    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet]
+        public dynamic GetProcessPayment(int card_no, int card_limit, int processing_charge)
+        {
+            PaymentDetails payment = new PaymentDetails();
+
+            int bal_amount = card_limit;
+
+            bal_amount = bal_amount - processing_charge;
+
+            return payment.ToList();
+
+
+        }
+    }
+}
---
+++ Model/PaymentDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payment.Model
+{
+    public class PaymentDetails
+    {
+        public int bal_amount { get; set; }
+
+        public string Message { get; set; }
+
+        internal dynamic ToList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
