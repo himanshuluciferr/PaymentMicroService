1b2c303949cbd955d247a17c207c97fe052d07be
Groupcreation <groupcreation100@gmail.com>
2020-08-02 19:06:41 +0530

    Read and write in csv file

--- Controllers/ProcessPaymentController.cs
+++ Controllers/ProcessPaymentController.cs
@@ -13,98 +13,69 @@ using System.Threading;
 using Microsoft.AspNetCore.SignalR.Protocol;
 using System.Text;
 using System.IO;
+using System.Security.Permissions;
+using System.Security;
+using CsvHelper;
+using System.Globalization;
 
 namespace Payment.Controllers
 
-{    [Route("api/[controller]")]
+{
+    [Route("api/[controller]")]
     [ApiController]
     public class ProcessPaymentController : ControllerBase
     {
         static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(ProcessPaymentController));
-       /* List<TransactionRecords> trans = new List<TransactionRecords>() {
-                                             new TransactionRecords{CreditCardNumber=123, CreditLimit=20000,Count=0 },
-                                             new TransactionRecords{CreditCardNumber=122, CreditLimit=30000,Count=0 }
-
-                                             };
-       */
         [HttpGet]
         public dynamic ProcessPayment(long CreditCardNumber, int CreditLimit, int ProcessingCharge)
         {
 
             _log4net.Info("Payment initiated");
             PaymentDetails payment = new PaymentDetails();
-            
             payment.CurrentBalance = CreditLimit;               //current balance initiated
-
-            /* var CSVFile = new CsvFileDescription
-             {
-                 SeparatorChar = ',',
-                 FirstLineHasColumnNames = true
-             };
-
-             var CSVFile2 = new CsvFileDescription
-             {
-                 SeparatorChar = '\t',
-                 FirstLineHasColumnNames = true, 
-                 FileCultureName = "nl-NL"
-             };*/
-                int Count = 0;
-            // var CSV = new CsvContext();
-            //var Trans = File.ReadAllLines(@"Linq.csv").Select(x => x.Split(','));
-            /* var Trans = from values in CSV.Read<TransactionRecords>( path,CSVFile)
-                         select new
-                         {
-                             CreditCardNumber = values.CreditCardNumber,
-                             CreditLimit = values.CreditLimit,
-                             Count = values.Count
-                         };
-
-             */
-            /*var count = Trans.Select(count => count.Count);
-            foreach (int c in count)
+            int Count = 0;
+            using (var reader = new StreamReader("./TransactionRecord.csv"))       //csv file read
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
-                Count += c;
+                var count = csv.GetRecords<TransactionRecords>().Where(x => x.CreditCardNumber == CreditCardNumber).Select(x => x.Count);
+                foreach (int counter in count)
+                {
+                    Count += counter;
+                }
+                if (Count == 1)
+                {
+                    var limit = csv.GetRecords<TransactionRecords>().Where(x => x.CreditCardNumber == CreditCardNumber).Select(x => x.CreditLimit);
+                    foreach (int Limit in limit)
+                    {
+                        payment.CurrentBalance = Limit;
+                    }
+                    payment.Message = "Only one transaction per card allowed";
+                    return payment;
+                }
+                reader.Close();
             }
-            */
-            /*var val = from c in trans
-                      where c.CreditCardNumber == CreditCardNumber
-                      select c.Count;
-            */
-            /*foreach(int c in val)
+
+
+            if (payment.CurrentBalance >= ProcessingCharge)
             {
-                Count += c; 
-            }*/
-           
-            if (Count > 0)
-            {
-                payment.Message = "one transaction for one card";
-                
+                payment.CurrentBalance -= ProcessingCharge;
+                payment.Message = "Successful";
+                var records = new List<TransactionRecords>
+                {
+                    new TransactionRecords { CreditCardNumber = CreditCardNumber, CreditLimit = payment.CurrentBalance, Count = 1 }
+                };
+                using (var writer = new StreamWriter("./TransactionRecord.csv"))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))          
+                {
+                    csv.WriteRecords(records);                   //csv file written
+                    writer.Close();
+                }
             }
             else
             {
-                if (payment.CurrentBalance >= ProcessingCharge)
-                {
-                    payment.CurrentBalance -= ProcessingCharge;
-                    payment.Message = "Successful";
-
-                    //trans.RemoveAll(card =>card.CreditCardNumber==CreditCardNumber);
-                    //TransactionRecords records = new TransactionRecords();
-                    //records.CreditCardNumber = CreditCardNumber;
-                    //records.CreditLimit = payment.CurrentBalance;
-                    //records.Count = 1;
-                    //trans.Add(records);
-
-                     //trans.Where(w => w.CreditCardNumber == CreditCardNumber).ToList().ForEach(i => i.Count = 1);
-                     //trans.Where(w => w.CreditCardNumber == CreditCardNumber).ToList().ForEach(i => i.CreditLimit = payment.CurrentBalance);
-                    
-                }
-
-                else
-                {
-                    payment.Message = "Failed";      // message generated
-                }
+                payment.Message = "Failed";      // message generated
             }
-            return payment;             // returns message & balance amount
+            return payment;   // returns message & balance amount
         }
     }
-    }
+}
