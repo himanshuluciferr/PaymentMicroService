fdfdedd8d080035ced3870819457cf5dc2f8c96a
812620 <joshianjali21098@gmail.com>
2020-08-07 12:27:08 +0530

    csv removed

--- Repository/Payment.cs
+++ Repository/Payment.cs
@@ -16,8 +16,9 @@ namespace Payment.Repository
 
             PaymentDetails payment = new PaymentDetails();
             payment.CurrentBalance = det.CreditLimit;               //current balance initiated
-            int Count = 0;
-            using (var reader = new StreamReader("TransactionRecord.csv"))       //csv file read
+           
+            // int Count = 0;
+           /* using (var reader = new StreamReader("TransactionRecord.csv"))       //csv file read
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
                 var count = csv.GetRecords<TransactionRecords>().Where(x => x.CreditCardNumber == det.CreditCardNumber).Select(x => x.Count);
@@ -37,13 +38,13 @@ namespace Payment.Repository
                 }
                 reader.Close();
             }
-
+           */
 
             if (payment.CurrentBalance >= det.ProcessingCharge)
             {
                 payment.CurrentBalance -= det.ProcessingCharge;
                 payment.Message = "Successful";
-                var records = new List<TransactionRecords>
+                /*var records = new List<TransactionRecords>
                 {
                     new TransactionRecords { CreditCardNumber = det.CreditCardNumber, CreditLimit = payment.CurrentBalance, Count = 1 }
                 };
@@ -52,7 +53,7 @@ namespace Payment.Repository
                 {
                     csv.WriteRecords(records);                   //csv file written
                     writer.Close();
-                }
+                }*/
             }
             else
             {
