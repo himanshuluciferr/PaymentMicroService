7aec6a58d7472c8de5e94272a80f95566bb74c82
812620 <joshianjali21098@gmail.com>
2020-08-07 10:34:22 +0530

    csv file location changed

--- Repository/Payment.cs
+++ Repository/Payment.cs
@@ -16,22 +16,53 @@ namespace Payment.Repository
 
             PaymentDetails payment = new PaymentDetails();
             payment.CurrentBalance = det.CreditLimit;               //current balance initiated
+            int Count = 0;
+            using (var reader = new StreamReader("./bin/Debug/netcoreapp3.1/TransactionRecord.csv"))       //csv file read
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var count = csv.GetRecords<TransactionRecords>().Where(x => x.CreditCardNumber == det.CreditCardNumber).Select(x => x.Count);
+                foreach (int counter in count)
+                {
+                    Count += counter;
+                }
+                if (Count == 1)
+                {
+                    var limit = csv.GetRecords<TransactionRecords>().Where(x => x.CreditCardNumber == det.CreditCardNumber).Select(x => x.CreditLimit);
+                    foreach (int Limit in limit)
+                    {
+                        payment.CurrentBalance = Limit;
+                    }
+                    payment.Message = "Only one transaction per card allowed";
+                    return payment;
+                }
+                reader.Close();
+            }
 
 
             if (payment.CurrentBalance >= det.ProcessingCharge)
             {
                 payment.CurrentBalance -= det.ProcessingCharge;
                 payment.Message = "Successful";
+                var records = new List<TransactionRecords>
+                {
+                    new TransactionRecords { CreditCardNumber = det.CreditCardNumber, CreditLimit = payment.CurrentBalance, Count = 1 }
+                };
+                using (var writer = new StreamWriter("./bin/Debug/netcoreapp3.1/TransactionRecord.csv"))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(records);                   //csv file written
+                    writer.Close();
+                }
             }
-
             else
             {
                 payment.Message = "Failed";      // message generated
             }
-
             return payment;   // returns message & balance amount
         }
+
     }
+
 }
 
 
